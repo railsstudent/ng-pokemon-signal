c2921a756ac4796a79f1d3679884b09e3f6a8484
Connie Leung <cleung334@gmail.com>
2023-05-20 22:41:26 +0800

    feat: intial commit

--- projects/pokemon-signal-demo-3/src/app/app.component.ts
+++ projects/pokemon-signal-demo-3/src/app/app.component.ts
@@ -1,21 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { PokemonComponent } from './pokemon/pokemon/pokemon.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [PokemonComponent],
-  template: `<app-pokemon></app-pokemon>`,
+  imports: [CommonModule],
+  template: '<div>Testing</div>',
   styles: [`
     :host {
       display: block;
     }
-  `]
+  `],
 })
 export class AppComponent {
   constructor(titleService: Title) {
-    const title = 'Pokemon Signal Demo 2';
+    const title = 'Pokemon Signal Demo 3';
     titleService.setTitle(title);
   }
 }
