657d7f80c91baee0a90fc3bbe7a6084f331d30e3
Connie Leung <cleung334@gmail.com>
2023-05-24 23:52:11 +0800

    chore: copy pokemon codes

--- projects/pokemon-signal-demo-4/src/app/app.component.ts
+++ projects/pokemon-signal-demo-4/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Title } from '@angular/platform-browser';
-import { PokemonComponent } from './pokemon/pokemon.component';
+import { PokemonComponent } from './pokemon/pokemon/pokemon.component';
 
 @Component({
   selector: 'app-root',
