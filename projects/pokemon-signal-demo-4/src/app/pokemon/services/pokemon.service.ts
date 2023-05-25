b5a30680abbb0d2fb0bddbad1d87e324f706467c
Connie Leung <cleung334@gmail.com>
2023-05-25 23:55:56 +0800

    refactor: pokemon service to move inject to factory function

--- projects/pokemon-signal-demo-4/src/app/pokemon/services/pokemon.service.ts
+++ projects/pokemon-signal-demo-4/src/app/pokemon/services/pokemon.service.ts
@@ -1,53 +1,61 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { Observable, Subject, map, switchMap } from 'rxjs';
 import { FlattenPokemon, Pokemon } from '../interfaces/pokemon.interface';
-import { HttpClient } from '@angular/common/http';
-import { toSignal } from '@angular/core/rxjs-interop';
+
+const EMPTY_POKEMON: FlattenPokemon = {
+  id: -1,
+  name: '',
+  height: -1,
+  weight: -1,
+  back_shiny: '',
+  front_shiny: '',
+  abilities: [],
+  stats: [],
+};
+
+const retrievePokemonFn = () => {
+  const httpClient = inject(HttpClient);
+  return (id: number) => httpClient.get<Pokemon>(`https://pokeapi.co/api/v2/pokemon/${id}`);
+}
+
+const pokemonTransformer = (pokemon: Pokemon) => {
+  const abilities = pokemon.abilities.map((ability) => ({
+    name: ability.ability.name,
+    is_hidden: ability.is_hidden
+  }));
+
+  const stats = pokemon.stats.map((stat) => ({
+    name: stat.stat.name,
+    effort: stat.effort,
+    base_stat: stat.base_stat,
+  }));
+
+  return {
+    id: pokemon.id,
+    name: pokemon.name,
+    height: pokemon.height,
+    weight: pokemon.weight,
+    back_shiny: pokemon.sprites.back_shiny,
+    front_shiny: pokemon.sprites.front_shiny,
+    abilities,
+    stats,
+  }
+} 
+
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
-  private readonly httpClient = inject(HttpClient);
   private readonly pokemonIdSub = new Subject<number>();
+  private retrievePokemon = retrievePokemonFn();
   private readonly pokemon$: Observable<FlattenPokemon> =  this.pokemonIdSub
     .pipe(
-      switchMap((id) => this.httpClient.get<Pokemon>(`https://pokeapi.co/api/v2/pokemon/${id}`)),
-      map((pokemon) => {
-        const abilities = pokemon.abilities.map((ability) => ({
-          name: ability.ability.name,
-          is_hidden: ability.is_hidden
-        }));
-
-        const stats = pokemon.stats.map((stat) => ({
-          name: stat.stat.name,
-          effort: stat.effort,
-          base_stat: stat.base_stat,
-        }));
-
-        return {
-          id: pokemon.id,
-          name: pokemon.name,
-          height: pokemon.height,
-          weight: pokemon.weight,
-          back_shiny: pokemon.sprites.back_shiny,
-          front_shiny: pokemon.sprites.front_shiny,
-          abilities,
-          stats,
-        }
-      }
-    )
-  );
-  pokemon = toSignal(this.pokemon$, { initialValue: {
-      id: -1,
-      name: '',
-      height: -1,
-      weight: -1,
-      back_shiny: '',
-      front_shiny: '',
-      abilities: [],
-      stats: [],
-    } as FlattenPokemon
-  })
+      switchMap((id) => this.retrievePokemon(id)),
+      map((pokemon) => pokemonTransformer(pokemon))
+    );
+  pokemon = toSignal(this.pokemon$, { initialValue: EMPTY_POKEMON });
 
   updatePokemonId(pokemonId: number) {
     this.pokemonIdSub.next(pokemonId); 
