f92e6ea31d7b3735dc900b4697c8169c151663e2
Connie Leung <cleung334@gmail.com>
2023-06-14 22:52:56 +0800

    chore: delete boiler plate codes

--- projects/pokemon-signal-demo-7/src/app/app.component.ts
+++ projects/pokemon-signal-demo-7/src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Title } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
+import { PokemonComponent } from './pokemon/pokemon/pokemon.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  imports: [PokemonComponent, RouterOutlet],
+  template: `<app-pokemon></app-pokemon>`,
+  styles: [`
+    :host {
+      display: block;
+    }
+  `],
 })
 export class AppComponent {
-  title = 'pokemon-signal-demo-7';
+  constructor(titleService: Title) {
+    titleService.setTitle('Pokemon Signal Demo 7');
+  }
 }
