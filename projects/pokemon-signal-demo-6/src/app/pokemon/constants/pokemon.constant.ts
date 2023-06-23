91d5a5d08c7ee12e8f9c41b348bfdde55a6bda7f
Connie Leung <cleung334@gmail.com>
2023-06-24 01:22:28 +0800

    chore: delete injector and injection tokens

--- projects/pokemon-signal-demo-6/src/app/pokemon/constants/pokemon.constant.ts
+++
@@ -1,5 +0,0 @@
-import { InjectionToken } from '@angular/core';
-import { Ability, Statistics } from '../interfaces/pokemon.interface';
-
-export const POKEMON_STATS_TOKEN = new InjectionToken<Statistics[]>('pokemon_stats_token');
-export const POKEMON_ABILITY_TOKEN = new InjectionToken<Ability[]>('pokemon_ability_token');
