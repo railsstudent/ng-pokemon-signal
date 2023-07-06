138aac5c2de17e073d742643ff0d6ac3759f036e
Connie Leung <cleung334@gmail.com>
2023-07-06 23:52:57 +0800

    refactor:   delete most rxjs code in pokemonTabComponent

--- projects/pokemon-signal-demo-8/src/app/pokemon/interfaces/pokemon.interface.ts
+++ projects/pokemon-signal-demo-8/src/app/pokemon/interfaces/pokemon.interface.ts
@@ -24,20 +24,6 @@ export interface Pokemon {
   }[]
 }
 
-export type FlattenPokemon = Omit<Pokemon, 'sprites'| 'stats' | 'abilities'> & {
-  back_shiny: string;
-  front_shiny: string;
-  stats: {
-    name: string;
-    base_stat: number;
-    effort: number;
-  }[];
-  abilities: {
-    name: string;
-    is_hidden: boolean;
-  }[];
-}
-
 export interface Statistics {
   name: string;
   effort: number;
