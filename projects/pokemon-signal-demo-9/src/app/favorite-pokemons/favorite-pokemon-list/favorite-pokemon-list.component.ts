e02f14ca2421c24aebaf2154f2b2ee59d1e45c66
Connie Leung <cleung334@gmail.com>
2023-09-13 14:10:51 +0800

    chore: add routerLinkActive

--- projects/pokemon-signal-demo-9/src/app/favorite-pokemons/favorite-pokemon-list/favorite-pokemon-list.component.ts
+++ projects/pokemon-signal-demo-9/src/app/favorite-pokemons/favorite-pokemon-list/favorite-pokemon-list.component.ts
@@ -1,17 +1,17 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-favorite-pokemon-list',
   standalone: true,  
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   template: `
     <h2>Display my favorite Pokemons</h2>
     <ul>
-      <li><a routerLink="/favorite-pokemons/pikachu" [queryParams]="{ isMainCharacter: true }">Pikachu</a></li>
-      <li><a routerLink="/favorite-pokemons/jigglypuff" [queryParams]="{ isMainCharacter: false }">Jigglypuff</a></li>
-      <li><a routerLink="/favorite-pokemons/seaking" [queryParams]="{ isMainCharacter: false }">Seaking</a></li>
-      <li><a routerLink="/favorite-pokemons/snorlax" [queryParams]="{ isMainCharacter: false }">Snorlax</a></li>
+      <li><a routerLink="/favorite-pokemons/pikachu" [queryParams]="{ isMainCharacter: true }" routerLinkActive="active">Pikachu</a></li>
+      <li><a routerLink="/favorite-pokemons/jigglypuff" [queryParams]="{ isMainCharacter: false }" routerLinkActive="active">Jigglypuff</a></li>
+      <li><a routerLink="/favorite-pokemons/seaking" [queryParams]="{ isMainCharacter: false }" routerLinkActive="active">Seaking</a></li>
+      <li><a routerLink="/favorite-pokemons/snorlax" [queryParams]="{ isMainCharacter: false }" routerLinkActive="active">Snorlax</a></li>
     </ul>
     <router-outlet></router-outlet>
   `,
