95053ed9c6104f0c830b94ac46b043fa4bb33db1
Connie Leung <cleung334@gmail.com>
2024-01-03 01:15:18 +0800

    Update pokemon-abilities.component.ts

--- projects/pokemon-signal-demo-5/src/app/pokemon/pokemon-abilities/pokemon-abilities.component.ts
+++ projects/pokemon-signal-demo-5/src/app/pokemon/pokemon-abilities/pokemon-abilities.component.ts
@@ -1,6 +1,6 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { NgFor, NgTemplateOutlet } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { PokemonService } from '../services/pokemon.service';
+import { Ability } from '../interfaces/pokemon.interface';
 
 @Component({
   selector: 'app-pokemon-abilities',
@@ -9,7 +9,7 @@ import { PokemonService } from '../services/pokemon.service';
   template: `
     <div style="padding: 0.5rem;">
       <p>Abilities</p>
-      <ng-container *ngTemplateOutlet="content; context: { $implicit: pokemon().abilities }"></ng-container>
+      <ng-container *ngTemplateOutlet="content; context: { $implicit: abilities }"></ng-container>
     </div>
     <ng-template #content let-abilities>
       <div *ngFor="let ability of abilities" class="abilities-container">
@@ -43,5 +43,6 @@ import { PokemonService } from '../services/pokemon.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PokemonAbilitiesComponent {
-  pokemon = inject(PokemonService).pokemon;
+  @Input({ required: true })
+  abilities!: Ability[];
 }
