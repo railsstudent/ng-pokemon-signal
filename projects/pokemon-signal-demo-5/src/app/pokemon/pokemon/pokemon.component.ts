e02b885afcd1c1193b42745a5d07bdfd13f861af
Connie Leung <cleung334@gmail.com>
2024-01-03 01:10:59 +0800

    refactor: use toSignal to convert Observable to Signal

--- projects/pokemon-signal-demo-5/src/app/pokemon/pokemon/pokemon.component.ts
+++ projects/pokemon-signal-demo-5/src/app/pokemon/pokemon/pokemon.component.ts
@@ -1,9 +1,22 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { DisplayPokemon } from '../interfaces/pokemon.interface';
 import { PokemonAbilitiesComponent } from '../pokemon-abilities/pokemon-abilities.component';
-import { PokemonControlsComponent } from '../pokemon-controls/pokemon-control.component';
+import { PokemonControlsComponent } from '../pokemon-controls/pokemon-controls.component';
 import { PokemonPersonalComponent } from '../pokemon-personal/pokemon-personal.component';
 import { PokemonStatsComponent } from '../pokemon-stats/pokemon-stats.component';
 import { PokemonService } from '../services/pokemon.service';
+
+const initialValue: DisplayPokemon = {
+  id: -1,
+  name: '',
+  height: 0,
+  weight: 0,
+  backShiny: '',
+  frontShiny: '',
+  abilities: [],
+  stats: [],
+}
 
 @Component({
   selector: 'app-pokemon',
@@ -16,9 +29,9 @@ import { PokemonService } from '../services/pokemon.service';
         <img [src]="pokemon().frontShiny" />
         <img [src]="pokemon().backShiny" />
       </div>
-      <app-pokemon-personal />
-      <app-pokemon-stats />
-      <app-pokemon-abilities />
+      <app-pokemon-personal [personalData]="personalData()" />
+      <app-pokemon-stats [stats]="pokemon().stats" />
+      <app-pokemon-abilities [abilities]="pokemon().abilities" />
     </div>
     <app-pokemon-controls />
   `,
@@ -43,5 +56,15 @@ import { PokemonService } from '../services/pokemon.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PokemonComponent {
-  pokemon = inject(PokemonService).pokemon;
+  pokemonService = inject(PokemonService);
+  pokemon = toSignal(this.pokemonService.pokemon$, { initialValue });
+  personalData = computed(() => {
+    const { id, name, height, weight } = this.pokemon();
+    return [
+      { text: 'Id: ', value: id },
+      { text: 'Name: ', value: name },
+      { text: 'Height: ', value: height },
+      { text: 'Weight: ', value: weight },
+    ];
+  });
 }
